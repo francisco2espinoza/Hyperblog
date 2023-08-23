8e761645d96994a847d2f0125110bd953bd976fd
Francisco Espinoza <francisco2espinoza@icloud.com>
2023-08-23 12:02:19 -0500

    avance-proyecto-gif

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,12 @@
+console.log('Hola mundo!!!!');
+const nombre = 'Fernando';
+let apellido = 'Dongo';
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    const nombre = 'Peter';
+    console.log(nombre)
+}
+console.log(valorDado);
---
+++ 03-gif-proyect/src/GifExpertApp.jsx
@@ -0,0 +1,16 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+
+        {/* titulo */}
+        <h1>GifExpertApp</h1>
+
+        {/* Imput */}
+
+        {/* Listado de Gif*/}
+            {/* Listado de Gif*/}
+
+    </>
+  )
+}
